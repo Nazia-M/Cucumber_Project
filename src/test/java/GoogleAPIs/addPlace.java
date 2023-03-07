06d7aab603b303e2497a91df975f9a2ff54bf6ed
Nazia <nazia862@gmail.com>
2023-03-07 20:58:17 +0530

    API

--- src/test/java/GoogleAPIs/addPlace.java
+++ src/test/java/GoogleAPIs/addPlace.java
@@ -1,35 +1,39 @@
 package GoogleAPIs;
+import GoogleAPIsPayload.payload;
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import org.testng.Assert;
+
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
 public class addPlace {
     public static void main(String[] args) {
 
         RestAssured.baseURI = "https://rahulshettyacademy.com";
 
-        given().log().all().queryParam("key","qaclick123").header("Content-Type","application/json")
-                .body("{\n" +
-                        "  \"location\": {\n" +
-                        "    \"lat\": -38.383494,\n" +
-                        "    \"lng\": 33.427362\n" +
-                        "  },\n" +
-                        "  \"accuracy\": 50,\n" +
-                        "  \"name\": \"Frontline house\",\n" +
-                        "  \"phone_number\": \"(+91) 983 893 3937\",\n" +
-                        "  \"address\": \"29, side layout, cohen 09\",\n" +
-                        "  \"types\": [\n" +
-                        "    \"shoe park\",\n" +
-                        "    \"shop\"\n" +
-                        "  ],\n" +
-                        "  \"website\": \"http://google.com\",\n" +
-                        "  \"language\": \"French-IN\"\n" +
-                        "}"
-                )
+        //From addPlace API - capture place_id
+        //Pass place_id to updatePlace API with new address
+        //getPlace to validate if new address is updated
+
+        String response = given().log().all().queryParam("key","qaclick123").header("Content-Type","application/json")
+                .body(payload.AddPlacePayload())
                 .when()
                 .post("/maps/api/place/add/json")
                 .then()
                 .log().all()
-                .assertThat().statusCode(200);
+                .assertThat().statusCode(208)
+                .body("scope", equalTo("APP"))  //Validate response body, scope parameter
+                .header("server", "Apache/2.4.41 (Ubuntu)") //validate response body, server parameter
+                .extract().response().asString();
+
+        Assert.assertEquals("Add Place API Response", response);
+
+        //JsonPath class takes String as input and converts it into Json, and helps to parsing jaon
+        JsonPath js = new JsonPath(response);
+        String place_Id = js.getString("place_id");
+
+
 
     }
 
